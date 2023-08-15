567fe84e8cc51584ea127d44e8a09a36a0011292
agsantoro <adrian.santoro@gmail.com>
2023-08-15 19:54:25 -0300

    RMD tasas

---
+++ RMD/RMD005_Tasas/script.R
@@ -0,0 +1,2 @@
+url = "http://datos.salud.gob.ar/dataset/27c588e8-43d0-411a-a40c-7ecc563c2c9f/resource/fab9e990-865c-43c4-a643-3dbc3b70a934/download/defunciones-ocurridas-y-registradas-en-la-republica-argentina-anos-2005-2021.csv"
+download.file(url, destfile = "defunciones.csv")
